6d93f252f6ca8bcb10e90473cc2792eb17b069ce
Andrew <andrewzkan@gmail.com>
2019-04-23 21:53:45 -0700

    made showpost component work with local data, added props to it from NewsFeed

--- src/NewsFeed.js
+++ src/NewsFeed.js
@@ -1,2 +1,27 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import MakePost from './MakePost.js';
+import ShowPost from './ShowPost.js';
+
+
+class NewsFeed extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      uid: 0,
+      displayNum: 0,
+      numPosts: 4
+    };
+  }
+  render() {
+    return (
+      <div>
+        <ShowPost uid={this.state.uid} displayNum={this.state.displayNum} />
+        <ShowPost uid={this.state.uid} displayNum={this.state.displayNum + 1} />
+        <ShowPost uid={this.state.uid} displayNum={this.state.displayNum + 2} />
+        <ShowPost uid={this.state.uid} displayNum={this.state.displayNum + 3} />
+      </div>
+    );
+  }
+}
+export default NewsFeed;
