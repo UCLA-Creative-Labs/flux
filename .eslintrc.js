f0a773e53787278529528f60d9a17b9f81f58386
alexanderqchen <aqchen@ucla.edu>
2019-04-24 00:15:16 -0700

    Setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+    "prettier/react"
+  ],
+  env: {
+    browser: true,
+    node: true
+  }
+};
