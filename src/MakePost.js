1315330c3acc2a1c6ca07f33a4864f6816cac236
Ryan Lam <ryan.lam53@gmail.com>
2019-04-23 21:49:56 -0700

    Added to MakePost files, added Firebase files

--- src/MakePost.js
+++ src/MakePost.js
@@ -1,2 +1,39 @@
 import React, { Component } from 'react';
+import { FirebaseContext } from '../Firebase';
+import './MakePost.css'
 import ReactDOM from 'react-dom';
+
+class MakePost extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            uid: this.props.userid,
+            text: "",
+            photo: ""
+        }
+    };
+
+    textInputHandler = event => {
+        this.setState({ text: event.target.value });
+    }
+
+    postSubmitHandler = () => {
+
+    }
+
+    render() {
+        console.log(this.state.text);
+        return (
+            <div>
+                <h1>Make a Post</h1>
+                <textarea className="post" type="text" onChange={this.textInputHandler} />
+                <div>
+                    <button onSubmit={this.postSubmitHandler}>Submit</button>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default MakePost;
