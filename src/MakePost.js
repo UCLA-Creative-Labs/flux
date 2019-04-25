f27b25eb88a3a51fcedb85ab421e400198185da7
Ryan Lam <ryan.lam53@gmail.com>
2019-04-25 12:13:18 -0700

    Updated MakePost

--- src/MakePost.js
+++ src/MakePost.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import { FirebaseContext } from '../Firebase';
+
 import './MakePost.css'
 import ReactDOM from 'react-dom';
 
 class MakePost extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             uid: this.props.userid,
             text: "",
-            photo: ""
+            photo: null,
+            likes: 0,
+            timestamp: null
         }
     };
 
@@ -17,16 +20,25 @@ class MakePost extends React.Component {
         this.setState({ text: event.target.value });
     }
 
+    fileUploadHandler = event => {
+        this.setState({ photo: event.target.files[0] })
+    }
+
     postSubmitHandler = () => {
 
     }
 
     render() {
+        console.log(this.state.photo);
         console.log(this.state.text);
         return (
             <div>
                 <h1>Make a Post</h1>
                 <textarea className="post" type="text" onChange={this.textInputHandler} />
+                <div>
+                    <input type="file" accept="image/*" onChange={this.fileUploadHandler} />
+
+                </div>
                 <div>
                     <button onSubmit={this.postSubmitHandler}>Submit</button>
                 </div>
