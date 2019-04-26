22e4e6083f6d865c0dca1ddedae11392d4d5687c
Andrew <andrewzkan@gmail.com>
2019-04-26 12:49:46 -0700

    got config file to work but not firebase for showposts

--- src/MakePost.js
+++ src/MakePost.js
@@ -1,51 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './MakePost.css'
-import ReactDOM from 'react-dom';
+import "./MakePost.css";
+import ReactDOM from "react-dom";
 
 class MakePost extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            uid: this.props.userid,
-            text: "",
-            photo: null,
-            likes: 0,
-            timestamp: null
-        }
+    this.state = {
+      uid: this.props.userid,
+      text: "",
+      photo: null,
+      likes: 0,
+      timestamp: null
     };
+  }
 
-    textInputHandler = event => {
-        this.setState({ text: event.target.value });
-    }
+  textInputHandler = event => {
+    this.setState({ text: event.target.value });
+  };
 
-    fileUploadHandler = event => {
-        this.setState({ photo: event.target.files[0] })
-    }
+  fileUploadHandler = event => {
+    this.setState({ photo: event.target.files[0] });
+  };
 
-    postSubmitHandler = () => {
+  postSubmitHandler = () => {};
 
-    }
-
-    render() {
-        console.log(this.state.photo);
-        console.log(this.state.text);
-        return (
-            <div>
-                <h1>Make a Post</h1>
-                <textarea className="post" type="text" onChange={this.textInputHandler} />
-                <div>
-                    <input type="file" accept="image/*" onChange={this.fileUploadHandler} />
-
-                </div>
-                <div>
-                    <button onSubmit={this.postSubmitHandler}>Submit</button>
-                </div>
-            </div>
-
-        );
-    }
+  render() {
+    console.log(this.state.photo);
+    console.log(this.state.text);
+    return (
+      <div>
+        <h1>Make a Post</h1>
+        <textarea
+          className="post"
+          type="text"
+          onChange={this.textInputHandler}
+        />
+        <div>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={this.fileUploadHandler}
+          />
+        </div>
+        <div>
+          <button onSubmit={this.postSubmitHandler}>Submit</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MakePost;
--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+<<<<<<< HEAD
 
 ReactDOM.render(<App />, document.getElementById("root"));
+=======
+import Firebase, { FirebaseContext } from "./Firebase";
+import NewsFeed from "./NewsFeed.js";
+/*ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>
+  , document.getElementById('root'));*/
+
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<NewsFeed />, document.getElementById("NewsFeed"));
+>>>>>>> got config file to work but not firebase for showposts
