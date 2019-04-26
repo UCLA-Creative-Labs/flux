88fed306877408c010b866db122c1b9234150e37
Connie <connie90503@gmail.com>
2019-04-26 16:21:39 -0700

    implemented Message component

---
+++ src/components/MessagingWindow/Message.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Message extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        var styling = this.props.sent ? styles.sent : styles.received
+        return (
+            <div style={styling}>{this.props.text}</div>
+        )
+    }
+}
+
+var styles = {
+    sent: {
+        width: '40%',
+        backgroundColor: 'blue'
+    },
+    received: {
+        width: '40%',
+        backgroundColor: 'white'
+    }
+}
+export default Message;
