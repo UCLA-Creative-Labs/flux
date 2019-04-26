9fe6ee20867a06d36524f40497928f9d60b87703
Andrew <andrewzkan@gmail.com>
2019-04-26 02:40:04 -0700

    added firebase but can't get it to load bc ./config

--- src/ShowPost.js
+++ src/ShowPost.js
@@ -6,18 +6,23 @@ class ShowPost extends Component {
   constructor(props, context) {
     super(props, context);
   }
-  pseudofirebasecode() {
-    //let lastPost = firebase.database.ref('posts').OrderByKey().limitToLast(8);
-    /*
-    return firebase.database().ref('/posts/' + numPosts - displayNum).once('value').then(function (snapshot) {
-      var text = (snapshot.val() && snapshot.val().text) || '';
-      var 
-      */
-  }
+
   render() {
+    let last8Posts = Firebase.database.ref("posts").OrderByKey().limitToLast(8); //get last 8 posts
+    last8Posts.on("value", function (snapshot) {
+      console.log(snapshot.val().post_id);
+      console.log(snapshot.val().uid);
+      console.log(snapshot.val().timestamp);
+      console.log(snapshot.val().text);
+      console.log(snapshot.val().photo);
+      console.log(snapshot.val().likes);
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
     return (
       <div id="ShowPostContainer" >
-        <h1>Post: {this.props.uid} {this.props.displayNum}</h1>
+        <h1>Post: post_id here</h1>
+        <p>user_id here</p>
         <p>Timestamp here</p>
         <p>Post text/links here</p>
         <p>Photos here</p>
