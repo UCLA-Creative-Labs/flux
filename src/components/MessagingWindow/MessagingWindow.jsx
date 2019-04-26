fec7c58361fe4ee150d07b239e8a295d5bdc049f
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-04-26 02:24:01 -0700

    Added Form to send messages!
    Currently lists all messages in state, in a disgusting way though with no differntiation based on sender!
    Listener has been set up in ComponentDidMount.
    Hitting the submit button on the form successfully updates Firebase.
    Functionalty to start a new conversation not implemented yet!

--- src/components/MessagingWindow/MessagingWindow.jsx
+++ src/components/MessagingWindow/MessagingWindow.jsx
@@ -4,36 +4,65 @@ import "./MessagingWindow.css";
 import Firebase from "firebase";
 
 class MessagingWindow extends Component {
-  constructor() {
-    super();
-    tempRef= Firebase.database().ref("users/"+this.props.user+"/friends/"+this.props.receiver);
-    this.tempRef.on("value", snapshot => {
-        this.conversationRef = Firebase.database().ref("conversations/"+snapshot.val());
-      });
+  constructor(props) {
+    super(props);
+    
 
     this.state = {
-      messages: {},
+      messages: [],
       user: this.props.user,
-      receiver: this.props.receiver
+      receiver: this.props.receiver,
+      text: '' 
     };
   }
 
   componentDidMount() {
-    this.conversationRef.on("value", snapshot => {
-      this.setState({
-        messages: snapshot.val()
+    this.tempRef= Firebase.database().ref("users/"+this.props.user+"/friends/"+this.props.receiver+"/");
+    this.tempRef.on("value", snapshot1 => {
+      Firebase.database().ref("/conversations/"+snapshot1.val()+"/messages/").on("value", snapshot2 => {
+        this.setState({
+          messages: snapshot2.val()
+        });
+        console.log(this.state);
+        this.conversationRef = Firebase.database().ref("/conversations/"+snapshot1.val()+"/messages/");
       });
+    
     });
   }
+
+  handleChange = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  
+
+  handleClick = e => {
+    e.preventDefault();
+
+    const message = {text: this.state.text, user_id: this.state.user}
+    this.conversationRef.push(message);
+    this.setState({
+      text: ""
+    });
+  };
 
 
   render() {
 
     return (
       <div>
-        <h1>Empty Messaging Window</h1>
+        <h1>Temporary Messaging Window</h1>
+
         {/* TODO: Display all messages in from state, left indented if received, right indented if sent (To be changed later!)*/}
+        {Object.keys(this.state.messages).map((m, i) => <li key={i}> {this.state.messages[m].text} </li>)}
+
         {/* TODO: Have a simple form with one text field and one submit button to send a message */}
+        <form>
+          <input type="text" onChange={this.handleChange} value={this.state.text} />
+          <button onClick={this.handleClick} type="submit">
+            Send!
+          </button>
+        </form>
       </div>
     );
   }
