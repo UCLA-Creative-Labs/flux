233155a2323716177ab9738bc47933a73f363bd6
Connie Chen <33800628+conniec218@users.noreply.github.com>
2019-04-28 16:18:37 -0700

    Connie_Nikil/messages (#13)
    * Initial Commit
    
    Created MessagingWindow Component. Added TODOs
    
    * Added Form to send messages!
    
    Currently lists all messages in state, in a disgusting way though with no differntiation based on sender!
    Listener has been set up in ComponentDidMount.
    Hitting the submit button on the form successfully updates Firebase.
    Functionalty to start a new conversation not implemented yet!
    
    * Styling Fixes!
    
    * implemented Message component
    
    * style fixes
    
    * bug fix
    
    * package.json
    
    * Final Cleanup
    
    Ready to be pulled!
    
    * package.json
    
    * yarn.lock
    
    * Initial Commit
    
    Created MessagingWindow Component. Added TODOs
    
    * Added Form to send messages!
    
    Currently lists all messages in state, in a disgusting way though with no differntiation based on sender!
    Listener has been set up in ComponentDidMount.
    Hitting the submit button on the form successfully updates Firebase.
    Functionalty to start a new conversation not implemented yet!
    
    * Styling Fixes!
    
    * implemented Message component
    
    * style fixes
    
    * bug fix
    
    * package.json
    
    * Final Cleanup
    
    Ready to be pulled!
    
    * package.json
    
    * yarn.lock
    
    * Initial Commit
    
    Created MessagingWindow Component. Added TODOs
    
    * Added Form to send messages!
    
    Currently lists all messages in state, in a disgusting way though with no differntiation based on sender!
    Listener has been set up in ComponentDidMount.
    Hitting the submit button on the form successfully updates Firebase.
    Functionalty to start a new conversation not implemented yet!
    
    * Styling Fixes!
    
    * implemented Message component
    
    * style fixes
    
    * bug fix
    
    * package.json
    
    * Final Cleanup
    
    Ready to be pulled!
    
    * yarn.lock
    
    * eslint error fixes
    
    * more eslint error fixes
    
    * eslint error fixes
    
    * fixes
    
    * fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import Firebase from "firebase";
 
-import Example from "./components/example";
-import ExampleFirebase from "./components/exampleFirebase";
 import { firebaseConfig } from "./config/firebase";
 import "./App.css";
+import MessagingWindow from "./components/MessagingWindow/MessagingWindow";
 
 class App extends Component {
   constructor() {
@@ -13,18 +12,12 @@ class App extends Component {
     if (!Firebase.apps.length) {
       Firebase.initializeApp(firebaseConfig);
     }
-
-    this.state = {
-      example: "this is a string acting as an example property"
-    };
   }
 
   render() {
-    const { example } = this.state;
     return (
       <div className="App">
-        <Example example={example} />
-        <ExampleFirebase />
+        <MessagingWindow user="1234" receiver="9876" /> {/* For testing! */}
       </div>
     );
   }
---
+++ src/components/MessagingWindow/MessagingWindow.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import "./MessagingWindow.css";
+import Firebase from "firebase";
+import PropTypes from "prop-types";
+import Message from "./Message";
+
+class MessagingWindow extends Component {
+  constructor(props) {
+    super(props);
+
+    const { user, receiver } = this.props;
+    this.state = {
+      messages: [],
+      user,
+      receiver,
+      text: "" // temporary local variable used to handle text from the "send-message" text field
+    };
+  }
+
+  componentDidMount() {
+    const { user, receiver } = this.state;
+    this.tempRef = Firebase.database().ref(
+      `users/${user}/friends/${receiver}/`
+    ); // temporary reference to extract conversation reference
+    this.tempRef.once("value", snapshot1 => {
+      const id = snapshot1.val();
+      Firebase.database()
+        .ref(`/conversations/${id}/messages/`)
+        .on("value", snapshot2 => {
+          this.setState({
+            messages: snapshot2.val() // gives complete list of messages in given conversation node
+          });
+          this.conversationRef = Firebase.database().ref(
+            `/conversations/${id}/messages/`
+          );
+        });
+    });
+  }
+
+  handleChange = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  handleClick = e => {
+    e.preventDefault();
+
+    const { text, user } = this.state;
+    const message = { text, user_id: user };
+    this.conversationRef.push(message);
+    this.setState({
+      text: ""
+    });
+  };
+
+  render() {
+    const { text, messages, user } = this.state;
+    return (
+      <div>
+        <h1>Temporary Messaging Window</h1>
+
+        {/* Display all messages from state, white bg if received, blue bg if sent (To be changed later!) */}
+        {Object.keys(messages).map(messageId =>
+          messages[messageId].user_id === user ? (
+            <Message key={messageId} text={messages[messageId].text} sent />
+          ) : (
+            <Message
+              key={messageId}
+              text={messages[messageId].text}
+              sent={false}
+            />
+          )
+        )}
+
+        {/* A Simple form with one text field and one submit button to send a message */}
+        <form>
+          <input type="text" onChange={this.handleChange} value={text} />
+          <button onClick={this.handleClick} type="submit">
+            Send!
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+MessagingWindow.propTypes = {
+  user: PropTypes.string.isRequired,
+  receiver: PropTypes.string.isRequired
+};
+
+export default MessagingWindow;
