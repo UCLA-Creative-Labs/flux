c74a8051bc081c03d8e8e8c6d3ec5fc72eb84d49
Connie Chen <connie90503@gmail.com>
2019-05-03 21:36:02 -0700

    eslint fixes

--- src/components/ProfilePage/FriendsList.jsx
+++ src/components/ProfilePage/FriendsList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+<<<<<<< 97df7aeacb97f1a983eed4dc1f97fc6c7735be5b
 const FriendsList = ({ friends }) => {
   return (
     <div>
@@ -11,6 +12,18 @@ const FriendsList = ({ friends }) => {
     </div>
   );
 };
+=======
+function FriendsList({ friends }) {
+  return (
+    <div>
+      Friends:
+      {Object.keys(friends).map(friendId => (
+        <div key={friendId}>{friendId}</div>
+      ))}
+    </div>
+  );
+}
+>>>>>>> eslint fixes
 
 FriendsList.propTypes = {
   friends: PropTypes.shape({}).isRequired
