eda2788f0f4b9d6589aab3eaef92719fd078f939
Connie <connie90503@gmail.com>
2019-05-03 20:51:09 -0700

    pull list of friendIds from firebse

--- src/components/ProfilePage/FriendsList.jsx
+++ src/components/ProfilePage/FriendsList.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 
 class FriendsList extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                Hello
-            </div>
-        )
-    }
+  render() {
+      const { friends } = this.props;
+      console.log(friends);
+      return (<div>
+          Friends:
+          {Object.keys(friends).map((friendId) =>
+            <div key={friendId}>{friendId}</div>
+            )}
+          </div>)
+      ;
+  }
 }
 
 export default FriendsList;
--- src/components/ProfilePage/index.jsx
+++ src/components/ProfilePage/index.jsx
@@ -1,34 +1,45 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import FriendsList from "./FriendsList";
+import firebaseWrapper from "../../firebaseWrapper";
 
 class ProfilePage extends Component {
-    //user_id will be passed in as props
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            likedPosts: [],
-            userPosts: [],
-            friends: [],
-            activeTab: ""
-        }
-    }
+    this.state = {
+    //   likedPosts: [],
+    //   userPosts: [],
+      friends: []
+    //   activeTab: ""
+    };
+  }
 
-    render() {
-        const { userId } = this.props;
-        return (
-            <div>
-                <div>Profile Picture here</div>
-                <h1>{userId}</h1>
-                <FriendsList/>
-            </div>
-        )
-    }
+  componentDidMount() {
+      const { userId } = this.props; 
+      const updateFriends = friends => {
+          this.setState({
+              friends
+          });
+      };
+      firebaseWrapper.getAllFriends(userId, updateFriends);
+  }
+
+  render() {
+    const { userId } = this.props;
+    const { friends } = this.state;
+    return (
+      <div>
+        <div>Profile Picture here</div>
+        <h1>{userId}</h1>
+        <FriendsList friends={friends}/>
+      </div>
+    );
+  }
 }
 
 ProfilePage.propTypes = {
-    userId: PropTypes.string.isRequired
-}
+  userId: PropTypes.string.isRequired
+};
 
 export default ProfilePage;
