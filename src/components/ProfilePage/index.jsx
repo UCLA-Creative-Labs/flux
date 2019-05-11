91697a23bc0c6b28ee6694d863ccac4532676b1c
Connie <connie90503@gmail.com>
2019-05-10 21:07:48 -0700

    profile page route set up

--- src/components/ProfilePage/index.jsx
+++ src/components/ProfilePage/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import FriendsList from "./FriendsList";
 import NewsFeed from "../NewsFeed";
 import firebaseWrapper from "../../firebaseWrapper";
