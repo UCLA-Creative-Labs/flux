d3d0a2febe7dac5f825d7946d109d86e4e517acb
Andrew <andrewzkan@gmail.com>
2019-05-16 18:21:52 -0700

    eslint .-.

--- src/components/NotificationPanel/NotificationBox.jsx
+++ src/components/NotificationPanel/NotificationBox.jsx
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
 import "./NotificationBox.css";
 import PropTypes from "prop-types";
-class NotificationBox extends Component {
-  render() {
-    const { text } = this.props;
-    const { color } = this.props;
-    return (
-      <div className="NotificationBox">
-        <div className="color_type" style={{ backgroundColor: color }} />
-        <div className="image" src="./image.png" />
-        <p>{text}</p>
-      </div>
-    );
-  }
-}
+
+const NotificationBox = ({ text, color }) => (
+  <div className="NotificationBox">
+    <div className="color_type" style={{ backgroundColor: color }} />
+    <div className="image" src="./image.png" />
+    <p>{text}</p>
+  </div>
+);
 
 NotificationBox.propTypes = {
   text: PropTypes.string.isRequired,
--- src/components/NotificationPanel/index.jsx
+++ src/components/NotificationPanel/index.jsx
@@ -3,27 +3,22 @@ import PropTypes from "prop-types";
 import "./styles.css";
 import NotificationBox from "./NotificationBox.jsx";
 
-class NotificationPanel extends Component {
-  render() {
-    const { notifications } = this.props;
-    return (
-      <div className="NotificationPanel">
-        {Object.keys(notifications)
-          .slice(0, 10)
-          .map(item => (
-            <NotificationBox
-              text={notifications[item].text}
-              color={notifications[item].color}
-              key={item}
-            />
-          ))}
-      </div>
-    );
-  }
-}
+const NotificationPanel = ({ notifications }) => (
+  <div className="NotificationPanel">
+    {Object.keys(notifications)
+      .slice(0, 10)
+      .map(item => (
+        <NotificationBox
+          text={notifications[item].text}
+          color={notifications[item].color}
+          key={item}
+        />
+      ))}
+  </div>
+);
 
 NotificationPanel.propTypes = {
-  notifications: PropTypes.array.isRequired
+  notifications: PropTypes.isRequired
 };
 
 export default NotificationPanel;
