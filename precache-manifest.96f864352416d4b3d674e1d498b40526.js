8aa11c3cb19968e9697d481c732daa7358eb76d1
alexanderqchen <aqchen@ucla.edu>
2019-05-17 20:27:16 -0700

    Updates

---
+++ precache-manifest.96f864352416d4b3d674e1d498b40526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4aec1659a60416b4a04",
+    "url": "/flux/static/css/main.fd7a497a.chunk.css"
+  },
+  {
+    "revision": "e4aec1659a60416b4a04",
+    "url": "/flux/static/js/main.7352cdf6.chunk.js"
+  },
+  {
+    "revision": "20778f6c5a6b05215ea3",
+    "url": "/flux/static/js/runtime~main.17cfcb3e.js"
+  },
+  {
+    "revision": "74a827765aca3cb76087",
+    "url": "/flux/static/js/2.be06d0fd.chunk.js"
+  },
+  {
+    "revision": "bc149fd627e6074f4337818fef1934c0",
+    "url": "/flux/index.html"
+  }
+];
