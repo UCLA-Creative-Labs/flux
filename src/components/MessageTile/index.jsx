f642c24a9e1a9954015274a2631198a3a2589d7f
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-05-21 14:39:43 -0700

    Created TIle Component

---
+++ src/components/MessageTile/index.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import firebaseWrapper from "../../firebaseWrapper";
+import Message from "./Message";
+import "./styles.css";
+
+class MessageTile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lastMessage: "",
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    // //Fetch user data!
+    // const updateMessages = (messages, prevRef) => {
+    //   this.setState({
+    //     messages,
+    //     prevConversationRef: prevRef
+    //   });
+    // };
+    // firebaseWrapper.listenForMessages(
+    //   //Listen for new message!
+    //   prevConversationRef,
+    //   conversationId,
+    //   updateMessages
+    // );
+  }
+
+  render() {
+    return (
+      <div className="tile-wrapper">
+        <div className="profile-picture">
+          <img />
+        </div>
+        <div className="user-content">
+          <div className="user-name">User</div>
+          <div className="message">Last Message</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+MessageTile.propTypes = {
+  userId: PropTypes.string.isRequired
+};
+
+export default MessageTile;
