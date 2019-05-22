458504ef5465b2bed3d51b3a67e92340db956099
alexanderqchen <aqchen@ucla.edu>
2019-05-21 17:03:47 -0700

    styled navbar in a very iffy way

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import "./styles.css";
 
 const Navbar = ({ userId }) => {
   return (
     <nav>
-      <Link to="/newsfeed">Home</Link>
-      <Link to="/messages">Messages</Link>
-      <Link to={`/user/${userId}`}>Profile</Link>
+      <ul>
+        <li>
+          <Link to="/newsfeed">Home</Link>
+        </li>
+        <li>
+          <Link to="/messages">Messages</Link>
+        </li>
+        <li id="control-blob">Filler for control blob</li>
+        <li>
+          <Link to="/notificationpanel">Notifications</Link>
+        </li>
+        <li>
+          <Link to={`/user/${userId}`}>Profile</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
