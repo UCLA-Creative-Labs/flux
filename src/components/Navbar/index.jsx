a9772736e3c2dcb634a34e89c8c2cc7bc414e476
Alexander Chen <aqchen@ucla.edu>
2019-05-23 15:45:18 -0700

    created navbar (#44)
    * styled navbar in a very iffy way
    
    * improved tab background image (makes this implementation method preeetty good)
    
    * navbar works surprisingly well
    
    * moved tab.svg file

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import "./styles.css";
 
-const Navbar = ({ userId }) => {
+const Navbar = ({ userId, activeTab }) => {
   return (
     <nav>
-      <Link to="/newsfeed">Home</Link>
-      <Link to="/messages">Messages</Link>
-      <Link to={`/user/${userId}`}>Profile</Link>
+      <ul>
+        <li className={activeTab === "home" ? "activeTab" : ""}>
+          <Link to="/newsfeed">Home</Link>
+        </li>
+        <li className={activeTab === "messages" ? "activeTab" : ""}>
+          <Link to="/messages">Messages</Link>
+        </li>
+        <li id="control-blob">Filler for control blob</li>
+        <li className={activeTab === "notifications" ? "activeTab" : ""}>
+          <Link to="/notificationpanel">Notifications</Link>
+        </li>
+        <li className={activeTab === "profile" ? "activeTab" : ""}>
+          <Link to={`/user/${userId}`}>Profile</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
 
 Navbar.propTypes = {
-  userId: PropTypes.string.isRequired
+  userId: PropTypes.string.isRequired,
+  activeTab: PropTypes.oneOf(["home", "messages", "notifications", "profile"])
+    .isRequired
 };
 
 export default Navbar;
