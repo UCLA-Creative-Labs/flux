3a2aca2a3bd4a34d099a5396326c47756010a761
Connie <connie90503@gmail.com>
2019-05-26 17:58:59 -0700

    added Curve prop validation (eslint)

--- src/components/ProfilePage/index.jsx
+++ src/components/ProfilePage/index.jsx
@@ -6,10 +6,22 @@ import firebaseWrapper from "../../firebaseWrapper";
 import "./styles.css";
 
 const Curve = ({ onClick1, className1, onClick2, className2 }) => (
-    <svg className="svg" width="1400" height="78.867516">
-        <path className={className1} ref={(ref) => { if (ref) ref.addEventListener("click", onClick1); }} d="M 0 0 L 0 28.867188 C 394.53125 107.77344 757.93205 -0.1059539 1114.7656 0 L 0 0 z M 1114.7656 0 C 1210.1568 0.02832431 1305.0781 7.7734375 1400 28.867188 L 1400 0 L 1114.7656 0 z "></path> 
-        <path className={className2} ref={(ref) => { if (ref) ref.addEventListener("click", onClick2); }} d="m 0,78.867513 v -50 c 500,99.999997 950,-100 1400,0 v 50 z"></path> 
-    </svg>
+  <svg className="svg" width="1400" height="78.867516">
+    <path
+      className={className1}
+      ref={ref => {
+        if (ref) ref.addEventListener("click", onClick1);
+      }}
+      d="M 0 0 L 0 28.867188 C 394.53125 107.77344 757.93205 -0.1059539 1114.7656 0 L 0 0 z M 1114.7656 0 C 1210.1568 0.02832431 1305.0781 7.7734375 1400 28.867188 L 1400 0 L 1114.7656 0 z "
+    />
+    <path
+      className={className2}
+      ref={ref => {
+        if (ref) ref.addEventListener("click", onClick2);
+      }}
+      d="m 0,78.867513 v -50 c 500,99.999997 950,-100 1400,0 v 50 z"
+    />
+  </svg>
 );
 
 class ProfilePage extends Component {
@@ -56,7 +68,6 @@ class ProfilePage extends Component {
   };
 
   toggleActiveTab = activeTab => {
-      console.log(activeTab);
     this.setState({
       activeTab
     });
@@ -114,15 +125,15 @@ class ProfilePage extends Component {
         </div>
 
         <div>
-            <div className="curve">
-                <Curve
-                    onClick1={() => {}}
-                    className1="hidden" 
-                    onClick2={() => this.toggleActiveTab("friendsList")} 
-                    className2="friendsListPath" 
-                />
-            </div>
-        
+          <div className="curve">
+            <Curve
+              onClick1={() => {}}
+              className1="hidden"
+              onClick2={() => this.toggleActiveTab("friendsList")}
+              className2="friendsListPath"
+            />
+          </div>
+
           <div
             className={friendsListClass}
             onClick={() => this.toggleActiveTab("friendsList")}
@@ -132,15 +143,15 @@ class ProfilePage extends Component {
           >
             <FriendsList friends={friends} />
           </div>
-          
+
           <div className="curve">
-                <Curve
-                    onClick1={() => this.toggleActiveTab("friendsList")}
-                    className1="friendsListPath"
-                    onClick2={() => this.toggleActiveTab("userPosts")}
-                    className2="userPostsPath"
-                />
-            </div>
+            <Curve
+              onClick1={() => this.toggleActiveTab("friendsList")}
+              className1="friendsListPath"
+              onClick2={() => this.toggleActiveTab("userPosts")}
+              className2="userPostsPath"
+            />
+          </div>
           <div
             className={userPostsClass}
             onClick={() => this.toggleActiveTab("userPosts")}
@@ -153,13 +164,13 @@ class ProfilePage extends Component {
           </div>
 
           <div className="curve">
-                <Curve
-                    onClick1={() => this.toggleActiveTab("userPosts")}
-                    className1="userPostsPath"
-                    onClick2={() => this.toggleActiveTab("likedPostsPath")}
-                    className2="likedPostsPath"
-                />
-            </div>
+            <Curve
+              onClick1={() => this.toggleActiveTab("userPosts")}
+              className1="userPostsPath"
+              onClick2={() => this.toggleActiveTab("likedPostsPath")}
+              className2="likedPostsPath"
+            />
+          </div>
           <div
             className={likedPostsClass}
             onClick={() => this.toggleActiveTab("likedPosts")}
@@ -180,6 +191,13 @@ ProfilePage.propTypes = {
   match: PropTypes.shape({}).isRequired,
   userId: PropTypes.string.isRequired,
   handleLogout: PropTypes.func.isRequired
+};
+
+Curve.propTypes = {
+  onClick1: PropTypes.func.isRequired,
+  className1: PropTypes.string.isRequired,
+  onClick2: PropTypes.func.isRequired,
+  className2: PropTypes.string.isRequired
 };
 
 export default ProfilePage;
