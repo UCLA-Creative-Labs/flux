446f18724439f342161961536bc97daf7295ec5e
alexanderqchen <aqchen@ucla.edu>
2019-05-29 14:28:55 -0700

    added icon circle`

--- src/components/Navbar/ControlBlob.jsx
+++ src/components/Navbar/ControlBlob.jsx
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import LikeIcon from "../../images/Navbar/like.svg";
+import DislikeIcon from "../../images/Navbar/dislike.svg";
 import "./styles.css";
 
 class ControlBlob extends Component {
   constructor() {
     super();
   }
+
+  generateIcons = () => {
+    const numIcons = 16;
+    const angleSpacing = 360 / numIcons;
+
+    let angleClasses = [];
+
+    for (let i = 0; i < numIcons; i++) {
+      const angle = i * angleSpacing;
+      angleClasses.push(angle.toString().replace(".", "_"));
+    }
+
+    return (
+      <div id="circle">
+        {angleClasses.map((angle, i) => {
+          console.log(angle);
+          return (
+            <img
+              src={i % 2 == 0 ? LikeIcon : DislikeIcon}
+              className={`icon deg${angle}`}
+              key={angle}
+            />
+          );
+        })}
+      </div>
+    );
+  };
+
   render() {
     return (
       <div id="control-blob">
-        <div id="circle" />
+        {this.generateIcons()}
         <div id="blob">
           {/* <textarea placeholder="PLEASE TYPE YOUR MESSAGE" /> */}
           WHAT'S ON
