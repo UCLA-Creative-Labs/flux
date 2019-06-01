33b926ed1bb6cd48a692d82b0e8b07ff384ddcb5
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-06-01 12:47:03 -0700

    Added Bubbles

--- src/components/MessagingWindow/Message.jsx
+++ src/components/MessagingWindow/Message.jsx
@@ -4,18 +4,37 @@ import "../../colors.css";
 
 const styles = {
   sent: {
-    width: "40%",
-    backgroundColor: "var(--light-teal)"
+    display: "inline-block",
+    boxSizing: "border-box",
+    height: "auto",
+    width: "auto",
+    //borderRadius: "50%",
+    // backgroundColor: "var(--light-teal)"
+    backgroundImage: "url(" + require("../../images/SentBubble.svg") + ")",
+    backgroundRepeat: "no-repeat"
   },
   received: {
-    width: "40%",
-    backgroundColor: "var(--blueberry)"
+    display: "inline-block",
+    boxSizing: "border-box",
+    height: "auto",
+    width: "auto",
+    //borderRadius: "50%",
+    // backgroundColor: "var(--blueberry)"
+    backgroundImage: "url(" + require("../../images/ReceivedBubble.svg") + ")",
+    backgroundRepeat: "no-repeat"
   }
 };
 
 function Message({ text, sent }) {
   const styling = sent ? styles.sent : styles.received;
-  return <div style={styling}>{text}</div>;
+  const bgsource = sent
+    ? "../../../images/SentBubble.svg"
+    : "../../../images/ReceivedBubble.svg";
+  return (
+    <div style={styling}>
+      <p style={{ textAlign: "center" }}>{text}</p>
+    </div>
+  );
 }
 
 Message.propTypes = {
