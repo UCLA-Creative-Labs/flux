bd8683e03d25408cdc2705983f28e7747b3dd412
Alexander Chen <aqchen@ucla.edu>
2019-06-01 17:59:27 -0700

    created loading page (#61)
    * created loading page
    
    * eslint fixes
    
    * added support for ff and ie

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import NotificationPanel from "./components/NotificationPanel";
 import Home from "./pages/Home";
 import Messages from "./pages/Messages";
 import Profile from "./pages/Profile";
+import Loading from "./components/Loading";
 import "./App.css";
 
 class App extends Component {
@@ -82,6 +83,7 @@ class App extends Component {
       <div className="App">
         <Router>
           <Route exact path="/" render={() => <Redirect to="/newsfeed" />} />
+          <Route path="/loading" component={Loading} />
           <Route
             path="/newsfeed"
             render={() => {
