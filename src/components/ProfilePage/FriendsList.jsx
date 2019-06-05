4047819c421040fe2eb78009c2ffe135f2c9f01a
alexanderqchen <aqchen@ucla.edu>
2019-06-05 09:16:05 -0700

    removed commented code

--- src/components/ProfilePage/FriendsList.jsx
+++ src/components/ProfilePage/FriendsList.jsx
@@ -1,52 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./FriendsList.css";
-// import { Link } from "react-router-dom";
 import FriendListing from "./FriendListing";
-// import firebaseWrapper from "../../firebaseWrapper";
-
-// class FriendListing extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       name: "",
-//       pictureURL: ""
-//     };
-//   }
-
-//   componentDidMount() {
-//     const { id } = this.props;
-//     const getName = (first, last) => {
-//       this.setState({
-//         name: `${first} ${last}`
-//       });
-//     };
-//     const getPictureURL = pictureURL => {
-//       this.setState({
-//         pictureURL
-//       });
-//     };
-//     firebaseWrapper.getName(id, getName);
-//     firebaseWrapper.getProfilePicture(id, getPictureURL);
-//   }
-
-//   render() {
-//     const { name, pictureURL } = this.state;
-//     const { id, action } = this.props;
-//     return (
-//       <div className="friendListing">
-//         <img alt="profile" className="picture" src={pictureURL} />
-//         <Link to={`/user/${id}`} className="friendName" onClick={action}>
-//           {name}
-//         </Link>
-//       </div>
-//     );
-//   }
-// }
-
-// FriendListing.propTypes = {
-//   id: PropTypes.string.isRequired
-// };
 
 const FriendsList = ({ friends, action }) => {
   return (
