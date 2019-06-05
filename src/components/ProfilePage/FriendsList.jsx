771e0bfa8cf06634c3fb0bc81896b94b180da550
Connie <connie90503@gmail.com>
2019-06-04 21:10:46 -0700

    friendslist styling

--- src/components/ProfilePage/FriendsList.jsx
+++ src/components/ProfilePage/FriendsList.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./FriendsList.css";
+import firebaseWrapper from "../../firebaseWrapper";
 
 const FriendsList = ({ friends }) => {
   return (
-    <div>
-      <h1>Friends:</h1>
-      {Object.keys(friends).map(friendId => (
-        <p key={friendId}>{friendId}</p>
-      ))}
+    <div className="container">
+      <h1 className="title">Friends</h1>
+      {Object.keys(friends).map(friendId => {
+          let profilePic;
+          const setProfilePic = picture => {
+            console.log(picture);
+            profilePic = picture;
+          }
+          firebaseWrapper.getProfilePicture(friendId, setProfilePic);
+          return (
+          <div className="friendListing">
+              <img alt="profile" className="picture" src="/static/media/default-profile-picture.995231b3.svg"/>
+              <p className="friendName" key={friendId}>{friendId}</p>
+            </div>
+      )})}
     </div>
   );
 };
